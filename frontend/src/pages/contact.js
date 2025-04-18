46240610ddf93859ca3b8e1f138a8844680d8a77
manimozhiyan23 <manimozhiyan88@gmail.com>
2025-04-18 12:32:26 +0530

    contact-finish

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,8 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot,faPhone,faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faInstagram,
+    faTwitter,
+    faFacebook,
+    faWhatsapp,
+    faPinterest,
+    faYoutube, } from '@fortawesome/free-brands-svg-icons';
+import abovefooter from "../images/above-footer.jpg";
 import "../styles/contact.css"
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.jpg';
+ import React from 'react';   //this is for set the google map
 function Contact(){
     return(
         <>
@@ -44,7 +52,12 @@ function Contact(){
 
             </div>
 
+            <div className='above-footer-image'>
+                    <img src={abovefooter}/>
+                </div>
 
+
+            <div className='contact-map'>
             <div className='contact-box'>
                 <div className='get-in-touch'>
                     <p>Get In Touch</p>
@@ -64,6 +77,46 @@ function Contact(){
                     <button type='submit'>submit</button>
                 </div>
             </div>
+            <div className='map'>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3504.7210216351154!2d77.25186707553948!3d28.548103975711015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce303269b736f%3A0x6a1dc1cf73e5568e!2sJasish!5e0!3m2!1sen!2sin!4v1744953630246!5m2!1sen!2sin"></iframe>
+            </div>
+
+            </div>
+
+
+
+            <footer className='contact-footer'>
+                <div>
+                    <p>© 2025 mani. All rights reserved.</p>
+                </div>
+
+                <div className='all-in-one-icons'>
+                <div className="contact-footer-icon">
+                <FontAwesomeIcon icon={faInstagram} />
+                </div>
+                <div className="contact-footer-icon">
+                <FontAwesomeIcon icon={faTwitter} />
+                </div>
+                <div className="contact-footer-icon">
+                <FontAwesomeIcon icon={faFacebook} />
+                </div>
+                <div className="contact-footer-icon">
+                <FontAwesomeIcon icon={faWhatsapp}/>
+                </div>
+                <div className="contact-footer-icon">
+                <FontAwesomeIcon icon={faPinterest}/>
+                </div>
+                <div className="contact-footer-icon">
+                <FontAwesomeIcon icon={faYoutube}/>
+                </div>
+                </div>
+                
+            </footer>
+
+    
+
+
+            
             
 
            
